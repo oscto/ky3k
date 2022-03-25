b7dd700f58f9ee4d3af2611381c1ef19df6a8578
Channing Duan <phpythoner@gmail.com>
2022-03-25 22:52:11 +0800

    add - ucfirst

--- string.go
+++ string.go
@@ -2,6 +2,7 @@ package ky3k
 
 import (
 	"strconv"
+	"unicode"
 )
 
 func StringToInt(s string) (int, error) {
@@ -10,4 +11,14 @@ func StringToInt(s string) (int, error) {
 
 func StringToInt64(s string) (int64, error) {
 	return strconv.ParseInt(s, 10, 64)
+}
+
+// UcFirst 首字母大写
+func UcFirst(str string) string {
+	for _, v := range str {
+		f := string(unicode.ToUpper(v))
+		return f + str[len(f):]
+	}
+
+	return ""
 }
